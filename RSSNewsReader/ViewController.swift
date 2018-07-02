199895664e44ea4fac9a7b40cd5481c25fd15c65
seungjun.lee <sigurnna@gmail.com>
2018-07-02 16:23:38 +0900

    - Side menubar 구현중...

---
+++ RSSNewsReader/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RSSNewsReader
+//
+//  Created by 이승준 on 2018. 7. 2..
+//  Copyright © 2018년 seungjun. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
