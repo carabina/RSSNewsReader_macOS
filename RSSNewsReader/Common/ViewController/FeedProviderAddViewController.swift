0ecb2629f3b8354b7f7a6f064f5057bb11abe5f0
seungjun.lee <sigurnna@gmail.com>
2018-07-12 22:55:16 +0900

    - Feed Provider 추가 코드 작성중.

--- RSSNewsReader/Common/ViewController/FeedProviderAddViewController.swift
+++ RSSNewsReader/Common/ViewController/FeedProviderAddViewController.swift
@@ -13,10 +13,14 @@ class FeedProviderAddViewController: NSViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        textField.delegate = self
     }
     
+    // MARK: - Button Action
+    
     @IBAction func didAddBtnTapped(_ sender: Any) {
-        
+        requestAddProvider()
     }
     
     @IBAction func didCancelBtnTapped(_ sender: Any) {
@@ -24,6 +28,40 @@ class FeedProviderAddViewController: NSViewController {
     }
 }
 
+// MARK: - NSTextFieldDelegate
+extension FeedProviderAddViewController: NSTextFieldDelegate {
+    override func controlTextDidEndEditing(_ obj: Notification) {
+        requestAddProvider()
+    }
+}
+
+// MARK: - Internal
+fileprivate extension FeedProviderAddViewController {
+    func requestAddProvider() {
+        guard !textField.stringValue.isEmpty else { return }
+        
+        var stringURL = textField.stringValue
+        
+        if !stringURL.hasPrefix("http://") {
+            stringURL.insert(contentsOf: "http://", at: stringURL.startIndex)
+        }
+        
+        NetworkService.shared.xml(url: stringURL) { (data, error) in
+            guard error == nil else {
+                // TODO: 에러 메시지를 띄워야함.. 별도의 에러 메시지 처리를 담당하는 클래스가 필요.
+                print(error?.localizedDescription)
+                return
+            }
+            
+            if let _data = data {
+                RSSXmlParser.shared.parseProvider(data: _data)
+                
+            }
+        }
+    }
+}
+
+// MARK: - ViewControllerInterface
 extension FeedProviderAddViewController: ViewControllerInterface {
     class func instance() -> Self {
         return instance(storyboardName: "FeedProviderAdd", identifier: "\(FeedProviderAddViewController.self)")
